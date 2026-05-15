2ed9c07f2486f289b34e4d599cc4e26e3cbea548
minstrike520 <minstrike520@gmail.com>
2026-05-15 16:23:51 +0800

    index.php

--- index.php
+++ index.php
@@ -21,34 +21,24 @@
 		<tr><th>編號</th><th>標題</th><th>導演</th><th>上映日期</th><th colspan="2">Action</th></tr>
 		
 		<?php
-			// ******** update your personal settings ******** 
-			$servername = "";
-			$username = "";
-			$password = "";
-			$dbname = "";
+			require_once 'config.php';	
+			
+			$sql = "SELECT * FROM $tablename;";
+			$result = $conn->query($sql);
 
-			// Connect MySQL server
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			
-			// set up char set
-			if (!$conn->set_charset("utf8")) {
-				printf("Error loading character set utf8: %s\n", $conn->error);
-				exit();
-			}
-			
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			} 
-			
-			// ******** update your personal settings ******** 
-			$sql = "";	// TODO: set up your sql query
-			$result = $conn->query($sql);	// Send SQL Query
-
-			if ($result->num_rows > 0) {	
-				// TODO: show the results
+			if ($result && $result->num_rows > 0) {	
+				while($row = $result->fetch_assoc()) {
+					echo "<tr>";
+					echo "<td>" . $row["id"] . "</td>";
+					echo "<td>" . $row["title"] . "</td>";
+					echo "<td>" . $row["director"] . "</td>";
+					echo "<td>" . $row["release_date"] . "</td>";
+					echo "<td><a href='update.php?id=" . $row["id"] . "'>Edit</a></td>";
+					echo "<td><a href='delete.php?id=" . $row["id"] . "' onclick='return confirm(\"Are you sure?\")'>Delete</a></td>";
+					echo "</tr>";
+				}
 			} else {
-				echo "0 results";
+				echo "<tr><td colspan='6' align='center'>0 results</td></tr>";
 			}
 		?>
 		
