d475355a7ac765f87d2e24fab40a4d3f7b42f2ca
minstrike520 <minstrike520@gmail.com>
2026-05-15 16:41:48 +0800

    delete.php

--- delete.php
+++ delete.php
@@ -1,28 +1,11 @@
 <?php
 
-// ******** update your personal settings ******** 
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
-
-// Connecting to and selecting a MySQL database
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if (!$conn->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $conn->error);
-    exit();
-}
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+require_once 'config.php';
 
 $id = $_GET['id'];
 
 if (isset($id)) {
-    $delete_sql = ""; // TODO 
+    $delete_sql = "DELETE FROM $tablename WHERE id='$id';";
 
 	if ($conn->query($delete_sql) === TRUE) {
         // echo "刪除成功!<a href='main.php'>返回主頁</a>";
